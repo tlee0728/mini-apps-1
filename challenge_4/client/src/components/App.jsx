8a6c14b12383f2c6cdf6bf39ec1758e1bc76ebbc
Timothy Lee <tlee728@berkeley.edu>
2019-06-27 19:09:49 -0700

    can now click any column to insert piece instead of specific spot

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -14,7 +14,22 @@ class App extends React.Component {
     };
     this.handleClick = this.handleClick.bind(this);
   }
-  handleClick (x, y) {
+
+  componentDidMount() {
+    axios
+      .post('/api')
+    
+  }
+
+  handleClick (col) {
+    var y = col;
+    var x;
+    for (var i = 5; i >= 0; i--) {
+      if (this.state.board[i][col] === 0) {
+        x = i; 
+        break;
+      }
+    }
     if (this.state.player === 1) {
       document.getElementById(`r${x}c${y}`).style.backgroundColor = 'red';
     } else {
@@ -37,10 +52,7 @@ class App extends React.Component {
           player: player,
           status: status
         })
-        if (this.state.status) {
-          axios
-            .post('/api')
-        }
+        
       })
       .catch( (err) => {'error placing piece'})
   }
@@ -57,63 +69,63 @@ class App extends React.Component {
         </tr>
         {/* row 1 */}
         <tr className="r0">
-          <td onClick={()=>this.handleClick(0, 0)} className="c0" id="r0c0"> </td>
-          <td onClick={()=>this.handleClick(0, 1)} className="c1" id="r0c1"></td>
-          <td onClick={()=>this.handleClick(0, 2)} className="c2" id="r0c2"></td>
-          <td onClick={()=>this.handleClick(0, 3)} className="c3" id="r0c3"></td>
-          <td onClick={()=>this.handleClick(0, 4)} className="c4" id="r0c4"></td>
-          <td onClick={()=>this.handleClick(0, 5)} className="c5" id="r0c5"></td>
-          <td onClick={()=>this.handleClick(0, 6)} className="c6" id="r0c6"></td>
+          <td onClick={()=>this.handleClick(0)} className="c0" id="r0c0"> </td>
+          <td onClick={()=>this.handleClick(1)} className="c1" id="r0c1"></td>
+          <td onClick={()=>this.handleClick(2)} className="c2" id="r0c2"></td>
+          <td onClick={()=>this.handleClick(3)} className="c3" id="r0c3"></td>
+          <td onClick={()=>this.handleClick(4)} className="c4" id="r0c4"></td>
+          <td onClick={()=>this.handleClick(5)} className="c5" id="r0c5"></td>
+          <td onClick={()=>this.handleClick(6)} className="c6" id="r0c6"></td>
         </tr>
 
         <tr className="r1">
-          <td onClick={()=>this.handleClick(1, 0)} className="c0" id="r1c0"></td>
-          <td onClick={()=>this.handleClick(1, 1)} className="c1" id="r1c1"></td>
-          <td onClick={()=>this.handleClick(1, 2)} className="c2" id="r1c2"></td>
-          <td onClick={()=>this.handleClick(1, 3)} className="c3" id="r1c3"></td>
-          <td onClick={()=>this.handleClick(1, 4)} className="c4" id="r1c4"></td>
-          <td onClick={()=>this.handleClick(1, 5)} className="c5" id="r1c5"></td>
-          <td onClick={()=>this.handleClick(1, 6)} className="c6" id="r1c6"></td>
+          <td onClick={()=>this.handleClick(0)} className="c0" id="r1c0"></td>
+          <td onClick={()=>this.handleClick(1)} className="c1" id="r1c1"></td>
+          <td onClick={()=>this.handleClick(2)} className="c2" id="r1c2"></td>
+          <td onClick={()=>this.handleClick(3)} className="c3" id="r1c3"></td>
+          <td onClick={()=>this.handleClick(4)} className="c4" id="r1c4"></td>
+          <td onClick={()=>this.handleClick(5)} className="c5" id="r1c5"></td>
+          <td onClick={()=>this.handleClick(6)} className="c6" id="r1c6"></td>
         </tr>
 
         <tr className="r2">
-          <td onClick={()=>this.handleClick(2, 0)} className="c0" id="r2c0"></td>
-          <td onClick={()=>this.handleClick(2, 1)} className="c1" id="r2c1"></td>
-          <td onClick={()=>this.handleClick(2, 2)} className="c2" id="r2c2"></td>
-          <td onClick={()=>this.handleClick(2, 3)} className="c3" id="r2c3"></td>
-          <td onClick={()=>this.handleClick(2, 4)} className="c4" id="r2c4"></td>
-          <td onClick={()=>this.handleClick(2, 5)} className="c5" id="r2c5"></td>
-          <td onClick={()=>this.handleClick(2, 6)} className="c6" id="r2c6"></td>
+          <td onClick={()=>this.handleClick(0)} className="c0" id="r2c0"></td>
+          <td onClick={()=>this.handleClick(1)} className="c1" id="r2c1"></td>
+          <td onClick={()=>this.handleClick(2)} className="c2" id="r2c2"></td>
+          <td onClick={()=>this.handleClick(3)} className="c3" id="r2c3"></td>
+          <td onClick={()=>this.handleClick(4)} className="c4" id="r2c4"></td>
+          <td onClick={()=>this.handleClick(5)} className="c5" id="r2c5"></td>
+          <td onClick={()=>this.handleClick(6)} className="c6" id="r2c6"></td>
         </tr>
 
         <tr className="r3">
-          <td onClick={()=>this.handleClick(3, 0)} className="c0" id="r3c0"></td>
-          <td onClick={()=>this.handleClick(3, 1)} className="c1" id="r3c1"></td>
-          <td onClick={()=>this.handleClick(3, 2)} className="c2" id="r3c2"></td>
-          <td onClick={()=>this.handleClick(3, 3)} className="c3" id="r3c3"></td>
-          <td onClick={()=>this.handleClick(3, 4)} className="c4" id="r3c4"></td>
-          <td onClick={()=>this.handleClick(3, 5)} className="c5" id="r3c5"></td>
-          <td onClick={()=>this.handleClick(3, 6)} className="c6" id="r3c6"></td>
+          <td onClick={()=>this.handleClick(0)} className="c0" id="r3c0"></td>
+          <td onClick={()=>this.handleClick(1)} className="c1" id="r3c1"></td>
+          <td onClick={()=>this.handleClick(2)} className="c2" id="r3c2"></td>
+          <td onClick={()=>this.handleClick(3)} className="c3" id="r3c3"></td>
+          <td onClick={()=>this.handleClick(4)} className="c4" id="r3c4"></td>
+          <td onClick={()=>this.handleClick(5)} className="c5" id="r3c5"></td>
+          <td onClick={()=>this.handleClick(6)} className="c6" id="r3c6"></td>
         </tr>
 
         <tr className="r4">
-          <td onClick={()=>this.handleClick(4, 0)} className="c0" id="r4c0"></td>
-          <td onClick={()=>this.handleClick(4, 1)} className="c1" id="r4c1"></td>
-          <td onClick={()=>this.handleClick(4, 2)} className="c2" id="r4c2"></td>
-          <td onClick={()=>this.handleClick(4, 3)} className="c3" id="r4c3"></td>
-          <td onClick={()=>this.handleClick(4, 4)} className="c4" id="r4c4"></td>
-          <td onClick={()=>this.handleClick(4, 5)} className="c5" id="r4c5"></td>
-          <td onClick={()=>this.handleClick(4, 6)} className="c6" id="r4c6"></td>
+          <td onClick={()=>this.handleClick(0)} className="c0" id="r4c0"></td>
+          <td onClick={()=>this.handleClick(1)} className="c1" id="r4c1"></td>
+          <td onClick={()=>this.handleClick(2)} className="c2" id="r4c2"></td>
+          <td onClick={()=>this.handleClick(3)} className="c3" id="r4c3"></td>
+          <td onClick={()=>this.handleClick(4)} className="c4" id="r4c4"></td>
+          <td onClick={()=>this.handleClick(5)} className="c5" id="r4c5"></td>
+          <td onClick={()=>this.handleClick(6)} className="c6" id="r4c6"></td>
         </tr>
 
         <tr className="r5">
-          <td onClick={()=>this.handleClick(5, 0)} className="c0" id="r5c0"></td>
-          <td onClick={()=>this.handleClick(5, 1)} className="c1" id="r5c1"></td>
-          <td onClick={()=>this.handleClick(5, 2)} className="c2" id="r5c2"></td>
-          <td onClick={()=>this.handleClick(5, 3)} className="c3" id="r5c3"></td>
-          <td onClick={()=>this.handleClick(5, 4)} className="c4" id="r5c4"></td>
-          <td onClick={()=>this.handleClick(5, 5)} className="c5" id="r5c5"></td>
-          <td onClick={()=>this.handleClick(5, 6)} className="c6" id="r5c6"></td>
+          <td onClick={()=>this.handleClick(0)} className="c0" id="r5c0"></td>
+          <td onClick={()=>this.handleClick(1)} className="c1" id="r5c1"></td>
+          <td onClick={()=>this.handleClick(2)} className="c2" id="r5c2"></td>
+          <td onClick={()=>this.handleClick(3)} className="c3" id="r5c3"></td>
+          <td onClick={()=>this.handleClick(4)} className="c4" id="r5c4"></td>
+          <td onClick={()=>this.handleClick(5)} className="c5" id="r5c5"></td>
+          <td onClick={()=>this.handleClick(6)} className="c6" id="r5c6"></td>
         </tr>
 
       </tbody>
